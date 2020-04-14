96fe643e1bfb2e4b6271fc26122ab5cfefd7bbb9
Felipe Oliveira <lipioliveira@gmail.com>
2020-04-13 22:54:52 -0300

    somar 2 numeros e trazer a soma

---
+++ Exercicios Curso em video Python/exercicio003.py
@@ -0,0 +1,5 @@
+numero1 = int(input('Insira um número:'))
+numero2 = int(input('Insira outro número:'))
+soma = numero1+numero2
+
+print('A soma do número {}, e do número {} é {}'.format(numero1,numero2, soma))
