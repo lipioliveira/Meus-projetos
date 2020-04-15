27f1a45f9d7daed54922b0aa067a8898ff9f7d54
Felipe Oliveira <lipioliveira@gmail.com>
2020-04-14 23:11:40 -0300

    concluído ex 005

---
+++ Exercicios Curso em video Python/exercicio005.py
@@ -0,0 +1,10 @@
+#Antecessores e Sucessores
+
+base = int(input('Insira um número:'))
+sucessor = base + 1
+antecessor = base - 1
+
+#Solução 1
+
+#print('Com base no número {}, o antecessor é {} e o sucessor é {}!'.format(base, antecessor, sucessor))
+print('Com base no número {}, o antecessor é {} e o sucessor é {}!'.format(base, (base-1), (base+1)))
---
+++ Exercicios Curso em video Python/exercicio006.py
@@ -0,0 +1,3 @@
+#Mostre o dobro, o triplo e a raiz quadrada de um número
+base = int(input('Insira um número:'))
+print('O dobro de {} é, o triplo é {} e a raiz quadrada é {}'.format(base*2, (base*3), (base*(1/2))))
