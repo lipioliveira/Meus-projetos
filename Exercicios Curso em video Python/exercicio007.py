7bc67b02b7e521f3356f4597f1c0c8c5ab53ba81
Felipe Oliveira <lipioliveira@gmail.com>
2020-04-15 20:41:36 -0300

    exercício de Média Aritimética executado

---
+++ Exercicios Curso em video Python/exercicio007.py
@@ -0,0 +1,4 @@
+#Média Aritimética
+nota1 = float(input('Qual a primeira nota?'))
+nota2 = float(input('Qual a segunda nota?'))
+print('A média da soma da {:.1f} e da {:.1f} é {:.1f}'.format(nota1, nota2, ((nota1+nota2)/2)))
